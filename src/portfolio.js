8e44d036a31b6ff68e707cd6280f8b47e1b080d0
Mustafiz04 <kaifee000@gmail.com>
2020-08-13 02:34:45 +0530

    change1

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,62 +7,62 @@ const settings = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Mustafiz kaifee",
+  logo_name: "MustafizKaifee",
+  nickname: "kaif",
   subTitle:
     "A passionate individual who always thrive to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
     "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+  portfolio_repository: "https://github.com/Mustafiz04",
 };
 
 const socialMediaLinks = [
-	/* Your Social Media Link */
-	// github: "https://github.com/ashutosh1919",
-	// linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-	// gmail: "ashutoshhathidara98@gmail.com",
-	// gitlab: "https://gitlab.com/ashutoshhathidara98",
-	// facebook: "https://www.facebook.com/laymanbrother.19/",
-	// twitter: "https://twitter.com/ashutosh_1919",
-	// instagram: "https://www.instagram.com/layman_brother/"
-  
-	{
-		name: "Github",
-		link: "https://github.com/ashutosh1919",
-		fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-		backgroundColor: '#181717' // Reference https://simpleicons.org/?q=github
-	},
-	{
-		name: "LinkedIn",
-		link: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-		fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-		backgroundColor: '#0077B5' // Reference https://simpleicons.org/?q=linkedin
-	},
-	{
-		name: "Gmail",
-		link: "mailto:ashutoshhathidara98@gmail.com",
-		fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-		backgroundColor: '#D14836' // Reference https://simpleicons.org/?q=gmail
-	},
-	{
-		name: "Twitter",
-		link: "https://twitter.com/ashutosh_1919",
-		fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
-		backgroundColor: '#1DA1F2' // Reference https://simpleicons.org/?q=twitter
-	},
-	{
-		name: "Facebook",
-		link: "https://www.facebook.com/laymanbrother.19/",
-		fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
-		backgroundColor: '#1877F2' // Reference https://simpleicons.org/?q=facebook
-	},
-	{
-		name: "Instagram",
-		link: "https://www.instagram.com/layman_brother/",
-		fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-		backgroundColor: '#E4405F' // Reference https://simpleicons.org/?q=instagram
-	},
+  /* Your Social Media Link */
+  // github: "https://github.com/ashutosh1919",
+  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // gmail: "ashutoshhathidara98@gmail.com",
+  // gitlab: "https://gitlab.com/ashutoshhathidara98",
+  // facebook: "https://www.facebook.com/laymanbrother.19/",
+  // twitter: "https://twitter.com/ashutosh_1919",
+  // instagram: "https://www.instagram.com/layman_brother/"
+
+  {
+    name: "Github",
+    link: "https://github.com/ashutosh1919",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  {
+    name: "Gmail",
+    link: "mailto:ashutoshhathidara98@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/ashutosh_1919",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/laymanbrother.19/",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/layman_brother/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
 ];
 
 const skills = {
