065c39c3b9057ab7a796aebd9136c76c07230ef8
Mustafiz04 <kaifee000@gmail.com>
2021-10-17 14:19:42 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-357bdaaf44b8cd3298f3.js"
+    "url": "webpack-runtime-24264053d587c6eac917.js"
   },
   {
     "url": "framework-6bcb7bb9b6581723c30e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5954e1605448a54229badcfcb4629acc"
+    "revision": "a0895263123821cca0d07fb7208734ee"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
